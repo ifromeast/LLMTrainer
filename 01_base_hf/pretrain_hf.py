33a58ade616998f4673bcc615021f6a3c9b8e557
KevinGit1 <dongsjtu@163.com>
2023-08-05 14:41:50 +0800

    init

---
+++ 01_base_hf/pretrain_hf.py
@@ -0,0 +1,81 @@
+
+import argparse
+import logging
+import math 
+from dataclasses import dataclass, field
+from typing import List, Optional, Tuple, Dict, Any
+
+import transformers
+from transformers import Trainer
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+from utils.data_utils import get_dataset, fault_tolerance_data_collator
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    """Arguments for models."""
+    model_config_path: Optional[str] = field(
+        default=None,
+        metadata={"help": ("The model name or config path")}, )
+    tokenizer_name_or_path: Optional[str] = field(
+        default=None,
+        metadata={"help": ("The tokenizer name or path")}, )
+
+@dataclass
+class DataArguments:
+    """Arguments for datasets."""
+    datasets: List[str] = field(
+        default=None,
+        metadata={'help': 'Path to the local training data.'}, )
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    """Arguments for the training loop."""
+    cache_dir: Optional[str] = field(default='/root/alpaca_test/cache_dir')
+
+    model_max_length: int = field(
+        default=2048,
+        metadata={'help': 'Maximum sequence length. Sequences will be right padded (and possibly truncated).',},)
+
+
+def main() -> None:
+    """Main training routine."""
+    parser = transformers.HfArgumentParser([TrainingArguments, ModelArguments, DataArguments])
+    training_args, model_args, data_args = parser.parse_args_into_dataclasses()
+
+    tokenizer = tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name_or_path)
+    train_dataset = get_dataset(tokenizer, training_args.model_max_length, training_args.cache_dir)
+
+    config = AutoConfig.from_pretrained(model_args.model_config_path)
+    model = AutoModelForCausalLM.from_config(config)
+    n_params = sum({p.data_ptr(): p.numel() for p in model.parameters()}.values())
+    logger.info(f"Training new model from scratch - Total size={n_params/2**20:.2f}M params")
+
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset['train'] if training_args.do_train else None,
+        tokenizer=tokenizer,
+        data_collator=fault_tolerance_data_collator,
+    )
+
+     # Training
+    if training_args.do_train:
+        train_result = trainer.train()
+
+        metrics = train_result.metrics
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+        trainer.save_model()
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ utils/data_utils.py
@@ -0,0 +1,109 @@
+
+from itertools import chain
+from typing import List, Dict, Any, Mapping
+import torch
+import numpy as np
+import logging
+from datasets import load_dataset
+
+logger = logging.getLogger(__name__)
+
+
+def tokenize_function(examples, tokenizer):
+    return tokenizer(examples['text'])
+   
+# Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
+def group_texts(examples, block_size):
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+def get_dataset(tokenizer, model_max_length, cache_dir):
+    try:
+        lm_datasets = datasets.load_from_disk(cache_dir, keep_in_memory=False)
+    except:
+        raw_dataset = load_dataset("wikipedia", "20220301.simple", split='train', cache_dir=cache_dir, keep_in_memory=False)
+        logger.info("dataset has been loaded!")
+        tokenized_dataset = raw_dataset.map(
+            tokenize_function,
+            batched=True,
+            num_proc=64,
+            remove_columns=raw_dataset.column_names,
+            load_from_cache_file=True,
+            keep_in_memory=False,
+            fn_kwargs={"tokenizer":tokenizer},
+            desc="Running tokenizer on dataset",
+        )
+        grouped_datasets = tokenized_dataset.map(
+            group_texts,
+            batched=True,
+            num_proc=64,
+            load_from_cache_file=True,
+            keep_in_memory=False,
+            fn_kwargs={'block_size':model_max_length},
+            desc=f"Grouping texts in chunks of {model_max_length}",
+        )
+        processed_dataset = grouped_datasets
+        
+        lm_datasets = processed_dataset.train_test_split(test_size = 0.01)
+        lm_datasets.save_to_disk(cache_dir)
+    # lm_datasets = lm_datasets['train']
+    return lm_datasets
+
+
+
+def fault_tolerance_data_collator(features: List) -> Dict[str, Any]:
+    if not isinstance(features[0], Mapping):
+        features = [vars(f) for f in features]
+    first = features[0]
+    batch = {}
+
+    # Special handling for labels.
+    # Ensure that tensor is created with the correct type
+    # (it should be automatically the case, but let's make sure of it.)
+    if "label" in first and first["label"] is not None:
+        label = first["label"].item() if isinstance(first["label"], torch.Tensor) else first["label"]
+        dtype = torch.long if isinstance(label, int) else torch.float
+        batch["labels"] = torch.tensor([f["label"] for f in features], dtype=dtype)
+    elif "label_ids" in first and first["label_ids"] is not None:
+        if isinstance(first["label_ids"], torch.Tensor):
+            batch["labels"] = torch.stack([f["label_ids"] for f in features])
+        else:
+            dtype = torch.long if isinstance(first["label_ids"][0], int) else torch.float
+            batch["labels"] = torch.tensor([f["label_ids"] for f in features], dtype=dtype)
+
+    # Handling of all other possible keys.
+    # Again, we will use the first element to figure out which key/values are not None for this model.
+
+    try:
+        for k, v in first.items():
+            if k not in ("label", "label_ids") and v is not None and not isinstance(v, str):
+                if isinstance(v, torch.Tensor):
+                    batch[k] = torch.stack([f[k] for f in features])
+                elif isinstance(v, np.ndarray):
+                    batch[k] = torch.tensor(np.stack([f[k] for f in features]))
+                else:
+                    batch[k] = torch.tensor([f[k] for f in features])
+    except ValueError: # quick fix by simply take the first example
+        for k, v in first.items():
+            if k not in ("label", "label_ids") and v is not None and not isinstance(v, str):
+                if isinstance(v, torch.Tensor):
+                    batch[k] = torch.stack([features[0][k]] * len(features))
+                elif isinstance(v, np.ndarray):
+                    batch[k] = torch.tensor(np.stack([features[0][k]] * len(features)))
+                else:
+                    batch[k] = torch.tensor([features[0][k]] * len(features))
+
+    return batch
